04f4b1d669277c584874897eb2a3e506ca14ae8d
aparkening <aaron.parkening@gmail.com>
2020-08-07 17:22:38 -0400

    Add droppable context to index and column

--- src/column.js
+++ src/column.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import Task from './task.js';
 import styled from 'styled-components';
+import { Droppable } from 'react-beautiful-dnd';
+
+import Task from './task.js';
 
 // Styles
 const Container = styled.div`
@@ -18,9 +20,21 @@ const TaskList = styled.div`
 
 export default class Column extends React.Component {
   render() {
-    return <Container>
-      <Title>{this.props.column.title}</Title>
-      <TaskList>{this.props.tasks.map(task => <Task key={task.id} task={task} />)}</TaskList>
-    </Container>
+    return (
+      <Container>
+        <Title>{this.props.column.title}</Title>
+        <Droppable droppableId={this.props.column.id}>
+          {provided => (
+            <TaskList
+              innerRef={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {this.props.tasks.map(task => <Task key={task.id} task={task} />)}
+              {provided.placeholder}
+            </TaskList>
+          )}
+        </Droppable>
+      </Container>
+    );
   }
 }
